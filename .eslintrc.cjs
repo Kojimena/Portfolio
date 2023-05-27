a688c8e163f45aed30c29404e55f8c90dd3f6d5b
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-05-26 18:23:08 -0600

    configuration prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,18 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'semi':['error','never'],
+    'max-len':['error',{ 'code':120}],
+    'prefer-arrow-callback': 'error',
+    'quotes': ['error', 'double'],
+    'react/function-component-definition': 'off',
   },
 }
