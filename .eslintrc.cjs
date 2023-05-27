6affb1a2646dea567aaaf3240a82e92cf6ddbded
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-05-26 18:37:32 -0600

    storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,26 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
   plugins: ['react-refresh'],
   rules: {
-    'semi':['error','never'],
-    'max-len':['error',{ 'code':120}],
+    'semi': ['error', 'never'],
+    'max-len': ['error', {
+      'code': 120
+    }],
     'prefer-arrow-callback': 'error',
     'quotes': ['error', 'double'],
-    'react/function-component-definition': 'off',
-  },
-}
+    'react/function-component-definition': 'off'
+  }
+};
