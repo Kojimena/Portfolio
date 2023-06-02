6c4eb9862f193bfcb582e0b81e2d7f6215ac3caf
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-06-01 22:46:22 -0600

    proyects

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react"
+import React, { useState } from "react"
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react"
 
@@ -10,11 +10,61 @@ import "swiper/css/pagination"
 import { Pagination } from "swiper"
 import styles from "./Projects.module.css"
 import { navigate } from "../../store"
+import ProjectImage from "../../components/ProjectImage/ProjectImage"
 
 const Projects = () => {
+  const [projectName, setProjectName] = useState(null)
+
   const handleNavigate = () => {
     navigate("/menu")
   }
+
+  const handleMouseEnter = (name) => {
+    setProjectName(name)
+  }
+
+  const handleMouseLeave = () => {
+    setProjectName(null)
+  }
+
+  const arrayProjects = [
+    {
+      name: "Coffee machine",
+      image: "/images/coffee.png",
+      url: "https://github.com/Kojimena/WEB-L4",
+      desc: "Maquina de café usando únicamente HTML y CSS",
+    },
+    {
+      name: "Memory Game",
+      image: "/images/memorygame.png",
+      url: "https://uvgenios.online/21199/WEB-L6/index.html",
+      desc: "Juego de memoria usando React",
+    },
+    {
+      name: "Maze Game",
+      image: "/images/maze.png",
+      url: "https://brilliant-crumble-59128a.netlify.app/",
+      desc: "Juego de laberinto usando React",
+    },
+    {
+      name: "Github in React",
+      image: "/images/github.png",
+      url: "https://incredible-lollipop-630aec.netlify.app",
+      desc: "Página principal de Github usando componentes de React",
+    },
+    {
+      name: "Calculator",
+      image: "/images/calculator.png",
+      url: "https://fabulous-crostata-9abe88.netlify.app",
+      desc: "Calculadora con implementación de vitest",
+    },
+    {
+      name: "Uniempleos",
+      image: "/images/uniempleos.png",
+      url: "https://sage-palmier-936be2.netlify.app/",
+      desc: "Página de empleos usando React",
+    },
+  ]
   return (
     <div className={styles.projectscontainer}>
       <div className={styles.header}>
@@ -34,23 +84,48 @@ const Projects = () => {
           modules={[Pagination]}
           className="mySwiper"
         >
-          <SwiperSlide>
+          <SwiperSlide
+            onMouseEnter={() => handleMouseEnter("Margherita")}
+            onMouseLeave={handleMouseLeave}
+          >
             <img
               src="/images/margherita.png"
               alt="img"
               className={styles.image}
             />
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide
+            onMouseEnter={() => handleMouseEnter("YVM")}
+            onMouseLeave={handleMouseLeave}
+          >
             <img src="/images/yvm.png" alt="img" className={styles.image} />
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide
+            onMouseEnter={() => handleMouseEnter("NacionSushi")}
+            onMouseLeave={handleMouseLeave}
+          >
             <img src="/images/nc.png" alt="img" className={styles.image} />
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide
+            onMouseEnter={() => handleMouseEnter("Paez")}
+            onMouseLeave={handleMouseLeave}
+          >
             <img src="/images/paez.png" alt="img" className={styles.image} />
           </SwiperSlide>
+          {projectName && (
+            <div className={styles.projectName}>{projectName}</div>
+          )}
         </Swiper>
+      </div>
+      <div className={styles.contentproyects}>
+        {arrayProjects.map((project) => (
+          <ProjectImage
+            name={project.name}
+            image={project.image}
+            url={project.url}
+            desc={project.desc}
+          />
+        ))}
       </div>
     </div>
   )
---
+++ src/components/ProjectImage/ProjectImage.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import styles from "./ProjectImage.module.css"
+
+const ProjectImage = ({ name, image, url, desc }) => {
+  return (
+    <div className={styles.projectimagecontainer}>
+      <h2>{name}</h2>
+      <div className={styles.projectimg}>
+        <button type="button" onClick={() => window.open(url)}>
+          <img src={image} alt="img" className={styles.image} />
+        </button>
+      </div>
+      <div className={styles.projectdesc}>
+        <p>{desc}</p>
+      </div>
+    </div>
+  )
+}
+
+ProjectImage.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+}
+
+export default ProjectImage
