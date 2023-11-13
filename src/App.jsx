364dc9cd5c5802a7e1a5173d8300edf43d36b66b
Jimena Hernández <her21199@uvg.edu.gt>
2023-11-13 12:53:39 -0600

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import "./App.css"
 import "./assets/fonts/fonts.css"
 import { StoreContext } from "storeon/react"
+import { Helmet } from "react-helmet"
 import store from "./store"
 import Page from "./Pages/index"
 
@@ -10,6 +11,23 @@ function App() {
     <StoreContext.Provider value={store}>
       <div className="App">
         <Page />
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Kojimena</title>
+        </Helmet>
+        <Helmet>
+          <meta name="description" content="Kojimena" />
+        </Helmet>
+        <Helmet>
+          <meta property="og:title" content="Kojimena" />
+          <meta
+            property="og:description"
+            content="Páginas web y diseño Guatemala"
+          />
+          <meta property="og:image" content="/images/logo.svg" />
+          <meta property="og:url" content="https://kojimena.com" />
+          <meta property="og:type" content="website" />
+        </Helmet>
       </div>
     </StoreContext.Provider>
   )
